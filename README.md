211367993c87662e9167613fcd43c8bd939ff6be
Mohammed Mosayed <mohammedmosayed@gmail.com>
2018-10-30 16:07:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# PCNWLab Classes 
+Object Orientated Introduction
+
+To complete this lab, do the following:
+
+1. Fork this repo
+2. Clone **your** forked repo and in terminal, open `pset#.js` file (or all js files that exist), solve the problems.
+3. Ensure your problems work properly!
+4. Open PR against [this](https://github.com/mmosayed/PCNWLab-Classes) repo.
---
+++ pset1.js
@@ -0,0 +1,44 @@
+/*  1
+    Define a @class called Rectangle
+    With @properties: length, height
+    With @methods: 
+        calculateArea() // Returns the area of the rectangle
+        calculatePerimeter() // Returns the perimeter of the rectangle
+        isSquare() // Returns true or false if this rectangle is a square or not
+*/
+
+
+
+
+/*  2
+    Define a @class called Circle
+    With @properties: radius
+    With @methods: 
+        calculateArea() // Returns the area of the circle
+        calculateDiameter() // returns the diameter of the circle
+        calculateCircumfrence() // Returns the circumfrence of the circle
+*/
+
+
+
+/*  3
+    Define a @class called Pet
+    With @properties: name, animal, greet
+    With @methods: 
+        greet() // Returns the pet's special greet. Eg. "Rexy the dog said Woof!"
+*/
+
+
+
+/*  4
+    Define a @class called Person
+    With @properties: name, age, pets
+    With @methods: 
+        profile() // Returns a greeting. Eg. "John is 30 years old."
+        addPet(name, animal, greet) // Adds a new pet for the person
+        meetPets() // Console.log() all of the person's pet's greets
+                   // Eg. 
+                   // John's pet Rexy the dog said Woof!
+                   // John's pet Annabelle the cat said Meow!
+                   // John's pet Elsie the cow said Moo!
+*/
