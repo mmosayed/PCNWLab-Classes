f7b23410299d46669a18b4c9e6bef89c24a5ad85
Robert Abreu <robertabreu@pursuit.org>
2018-11-03 16:40:16 -0400

    solution: problem 2 pset1: circle class

--- pset1.js
+++ pset1.js
@@ -46,7 +46,29 @@ console.log(square.isSquare());
         calculateCircumfrence() // Returns the circumfrence of the circle
 */
 
+class Circle {
+        constructor(radius){
+        this.radius = radius;
+        }
+        calculateArea(){
+                // A=πr2
 
+                return Math.PI * (this.radius ** 2)
+        }
+        calculateDiameter(){
+                // d=2r
+                return this.radius * 2
+        }
+        calculateCircumfrence(){
+                // C=2πr
+                return this.radius * Math.PI * 2
+        }
+}
+
+const circle = new Circle (5);
+console.log(circle.calculateArea());
+console.log(circle.calculateDiameter());
+console.log(circle.calculateCircumfrence());
 
 /*  3
     Define a @class called Pet
@@ -55,6 +77,17 @@ console.log(square.isSquare());
         greet() // Returns the pet's special greet. Eg. "Rexy the dog said Woof!"
 */
 
+class Pet {
+        constructor(name, animal, greeting){
+                this.name = name;
+                this.animal = animal;
+                this.greet = greeting;
+        }
+
+        greet(){
+                return `${this.name} the ${this.animal} said ${this.greeting}!`;
+        }
+}
 
 
 /*  4
@@ -69,3 +102,27 @@ console.log(square.isSquare());
                    // John's pet Annabelle the cat said Meow!
                    // John's pet Elsie the cow said Moo!
 */
+
+class Person {
+        constructor(name, age, pets = []){
+                this.name = name;
+                this.age = age;
+                this.pets = pets;
+        }
+        profile(){
+                return `${this.name} is ${this.age} years old.`;     
+        }
+        addPet(petName, animal, greeting){
+            this.pets.push(new Pet(petName, animal, greeting));    
+        }
+        meetPets(){
+             for(let i = 0; i < this.pets.length; i ++){
+                     console.log(`${this.name}'s pet ${this.pets[i].greet()}`)
+             }   
+        }
+}
+
+const rob = new Person('Robert', 29, []);
+console.log(rob.profile());
+rob.addPet('Bella', 'poodle', 'Woof' );
+console.log(rob.meetPets());
