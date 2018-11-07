6ec23a5b00b87792a90d564849698891bb9ad489
vincentgalarza <vincentgalarza@gmail.com>
2018-11-07 09:46:30 -0500

    completed PSET1. completed functions 1 & 2 in PSET2

--- pset1.js
+++ pset1.js
@@ -7,7 +7,33 @@
         isSquare() // Returns true or false if this rectangle is a square or not
 */
 
+class Rectangle {
+        constructor(length, height) {
+                this.length = length;
+                this.height = height;
+        }
 
+        calculateArea() {
+                return this.length * this.height;
+        }
+
+        calculatePerimeter() {
+                return (this.length + this.height) * 2;
+        }
+
+        isSquare() {
+                if (this.length === this.height) {
+                        return true;
+                }
+                return false;
+        }
+}
+
+const rect1 = new Rectangle(4,5);
+console.log(rect1);
+console.log(rect1.calculateArea());
+console.log(rect1.calculatePerimeter());
+console.log(rect1.isSquare());
 
 
 /*  2
@@ -19,7 +45,29 @@
         calculateCircumfrence() // Returns the circumfrence of the circle
 */
 
+class Circle {
+        constructor(radius) {
+                this.radius = radius;
+        }
 
+        calculateArea() {
+                return this.radius * (Math.PI ** 2);
+        }
+
+        calculateDiameter() {
+                return this.radius * 2
+        }
+
+        calculateCircumference() {
+                return Math.PI * this.calculateDiameter;
+        }
+}
+
+const circle1 = new Circle(5);
+console.log(circle1);
+console.log(circle1.calculateArea());
+console.log(circle1.calculateDiameter());
+console.log(circle1.calculateCircumference());
 
 /*  3
     Define a @class called Pet
@@ -28,7 +76,21 @@
         greet() // Returns the pet's special greet. Eg. "Rexy the dog said Woof!"
 */
 
+class Pet {
+        constructor(name, animal, speak) {
+                this.name = name;
+                this.animal = animal;
+                this.speak = speak;
+        }
 
+        greet() {
+                return `${this.name} the ${this.animal} said ${this.speak}!`
+        }
+}
+
+const pet1 = new Pet('Maya','dog','ruff');
+console.log(pet1);
+console.log(pet1.greet());
 
 /*  4
     Define a @class called Person
@@ -42,3 +104,48 @@
                    // John's pet Annabelle the cat said Meow!
                    // John's pet Elsie the cow said Moo!
 */
+
+const pets1 = [
+        {
+                name : 'maya',
+                animal : 'dog',
+                greet: 'ruff',
+        },
+
+        {
+                name: 'ya moms',
+                animal: 'a beast',
+                greet: 'gimme something to eat'
+        }
+]
+
+class Person {
+        constructor(name, age, pets=[]) {
+                this.name = name;
+                this.age = age;
+                this.pets = pets;
+        }
+
+        profile() {
+                return `${this.name} is ${this.age} years old.`
+        }
+
+        addPet() {
+                let newPetArray = [];
+                this.pets.forEach(pet => {
+                        newPetArray.push(new Pet(pet.name, pet.animal, pet.greet));
+                });
+
+                return newPetArray;
+
+        }
+
+        meetPets() {
+
+        }
+}
+
+const John = new Person('John', 20, pets1)
+console.log(John)
+console.log("=====WOW=====")
+console.log(John.addPet());
--- pset2.js
+++ pset2.js
@@ -14,8 +14,6 @@ const external_playlist = require('./playlist');
     holds the object we need.
 */
 
-
-
 /*  1
     Define a @class called Song
     With @properties: 
@@ -29,7 +27,57 @@ const external_playlist = require('./playlist');
                           Eg. "3 min 22 seconds"
 */
 
+class Song{
+	constructor(name, artists=[], length) {
+		this._setName(name);
+		this._setArtists(artists);
+		this.length = length;
+    }
+    
+    _setName(name) {
+        if (typeof name !== "string" || name.length < 1) {
+            throw new Error('name must be a string')
+        }
+        
+        this.name = name;
+    }
 
+    _setArtists(artists) {
+        if (!Array.isArray(artists) || artists.length === 0) {
+            throw new Error('artists must be an array')
+        }
+        
+        this.artists = artists;
+    }
+
+    _setName(name) {
+        if (typeof name !== "string" || name.length < 1) {
+            throw new Error('name must be a string')
+        }
+        
+        this.name = name;
+    }
+
+	getArtist() {
+		return this.artists[0];
+	}
+
+	getFeatures() {
+		return this.artists.slice(1);
+	}
+
+	getDuration() {
+		let minutes = Math.floor(this.length / 60);
+		let remainSecs = this.length % 60;
+		return `${minutes} min ${remainSecs} seconds`;
+	}
+}
+
+const lolSong = new Song("Fake Song LOL", ["Mr. lolZ", "hiya", "thisguytoo"], "324");
+console.log(lolSong);
+console.log(lolSong.getArtist());
+console.log(lolSong.getFeatures());
+console.log(lolSong.getDuration());
 
 /*  2
     Define a @class called Playlist
@@ -53,8 +101,94 @@ const external_playlist = require('./playlist');
                                  Eg. { name: "Drake", occurence: 4 }
 */
 
+class Playlist{
+	constructor(name, songs=[]) {
+		this.name = name;
+		this.songs = songs;
+	}
 
+	addSong(name, artists, length) {
+		const songObj = new Song(name, artists, length);
+		this.songs.push(songObj);
+	}
 
+	removeSong(name) {
+        this.songs = this.songs.reduce((acc, songObj) => {            
+            if (name !== songObj["name"]) {
+                acc = acc.concat(songObj);
+            }
+            return acc;
+        }, []);
+    }
+
+	getNumberOfSongs() {
+        return `Number of songs in playlist: ${this.songs.length}`;
+	}
+	
+	getNamesOfSongs() {
+        const nameArray = this.songs.reduce((acc, songObj) => {
+            return acc.concat(songObj["name"]);
+        }, []);
+        return `Songs in playlist: ${nameArray}`;
+	}
+
+	getTotalLength() {
+        const lengthSecs = this.songs.reduce((acc, songObj) => {
+            return acc = songObj["length"] + acc;
+        }, 0)
+        return lengthSecs;
+    }
+	
+	getTotalDuration() {
+        const totalLengthSecs = this.getTotalLength()
+        let minutes = Math.floor(totalLengthSecs / 60);
+		let remainSecs = totalLengthSecs % 60;
+		return `Total playlist length: ${minutes} min ${remainSecs} seconds`;
+    }
+    
+    getArtists() {
+        let artistsArray = this.songs.reduce((acc, currentSong) => {
+            let curSongArtists = currentSong.artists;
+            return acc.concat(curSongArtists.reduce((acc2, songArtist) => {
+                if (!acc.includes(songArtist)) {
+                    return acc2.concat(songArtist);
+                }
+                return acc2;
+            }, []));
+        }, []);
+        return artistsArray;
+    }
+
+         // for (let i = 0; i < curSongArtists; i++) {
+            //     if (!acc.includes(curSongArtists[i])) {
+            //         return acc.concat(curSongArtists[i]);
+            //     }
+            // }
+            // if (!acc.includes(currentSong.artists)) {
+            //     return acc.concat(currentSong.artists);
+            // }
+
+    // getMostCommonArtists() {
+    //     let mostCommonArtist
+    //     let bigArr = this.songs.reduce((acc, currentSong) => {
+    //         return acc.concat(currentSong.artists)
+    //     }
+    // }
+}
+
+const playlist_1 = new Playlist("The Best Playlist EVER, Vol. 1");
+playlist_1.addSong("Da Song", ["Me", "Myself", "I"], 340);
+playlist_1.addSong("Bear Song", ["Panda", "BrownB"], 242);
+playlist_1.addSong("Duck Song", ["DaffyD", "Mistah Fudd", "BB"], 414);
+playlist_1.addSong("Bug Song", ["Roach Master Roach", "BB"], 666);
+console.log(playlist_1.songs);
+playlist_1.removeSong("Da Song");
+console.log(playlist_1.songs);
+console.log(playlist_1.getNumberOfSongs());
+console.log(playlist_1.getTotalLength());
+console.log(playlist_1.getNamesOfSongs());
+console.log(playlist_1.getTotalDuration());
+console.log(playlist_1.getArtists());
 
 /* 3
     Define a ***GLOBAL VARIABLE***
